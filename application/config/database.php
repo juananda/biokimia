4394f06c8b0a506774ab6a6b32b989ecac617994
juananda <juanandasatria@gmail.com>
2017-08-23 14:40:25 +0700

    change config

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'biokimia',
+	'hostname' => 'mysql.hostinger.co.id',
+	'username' => 'u664670096_croco',
+	'password' => 'AwNfMP6uOQ3a',
+	'database' => 'u664670096_mydbs',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
